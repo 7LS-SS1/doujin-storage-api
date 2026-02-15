66abddf806e87a4af7fde4fbcae1c68c9e6874ce
7LS-SS1 <computer.ggez.ss1@gmail.com>
2026-02-15 17:28:48 +0700

    Revert "Add video type support and plugin-compatible API endpoints"
    This reverts commit 6dc83aa6d0a1f120a7664d2635401578ae947367.

--- prisma/migrations/20260214161000_add_video_type/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateEnum
-CREATE TYPE "VideoType" AS ENUM ('THAI_CLIP', 'AV_MOVIE');
-
--- AlterTable
-ALTER TABLE "videos"
-ADD COLUMN "type" "VideoType" NOT NULL DEFAULT 'THAI_CLIP';
-
--- CreateIndex
-CREATE INDEX "videos_type_idx" ON "videos"("type");
